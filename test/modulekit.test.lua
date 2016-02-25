a4efddbfc67244be02e1979157b42b63a230d822
bigbes <bigbes@gmail.com>
2016-02-25 20:33:19 +0300

    Update testing system to ctest

--- test/modulekit.test.lua
+++ test/modulekit.test.lua
@@ -10,10 +10,16 @@ for _, dir in pairs(dirs) do
 end
 
 local kit = require('modulekit')
+local tap = require('tap')
 
-require('tap').test('kit', function(test)
+local test = tap.test('modulekit tests')
+test:plan(1)
+
+test:test('kit', function(test)
     test:plan(3)
     test:is(kit.test(1), 11, "Lua function in init.lua")
     test:is(kit.functions.func(10, 15), 25, "Lua function in submodule")
     test:is(kit.cfunctions.func(10, 15), 25, "Lua/C function in submodule")
 end)
+
+os.exit(test:check() == true and 0 or -1)
